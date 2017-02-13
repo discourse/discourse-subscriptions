99758dcaa90375be921da37b7b1a8f44e30bc42d
Rimian Perkins <hello@rimian.com.au>
2017-02-14 10:28:07 +1100

    change routes

--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,10 @@
 # name: choice-plugin
 # about: Integrating Discourse with Stripe
-# version: 1.0.0
+# version: 1.0.1
 # authors: Rimian Perkins
 # url: https://github.com/choiceaustralia/choice-discourse
 
 gem 'stripe', '1.58.0'
-
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||  Rails.application.config_for(:stripe)['publishable_key'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY'] ||  Rails.application.config_for(:stripe)['secret_key']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 load File.expand_path('../lib/choice-discourse/engine.rb', __FILE__)
 
