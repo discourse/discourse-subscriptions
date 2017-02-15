1a20224481719c2cabd5068055367ed8c5db1f5c
Rimian Perkins <hello@rimian.com.au>
2017-02-15 12:36:38 +1100

    rename engine file

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,11 @@
-# name: choice-plugin
+# name: discourse-payments
 # about: Integrating Discourse with Stripe
 # version: 1.0.1
-# url: https://github.com/choiceaustralia/choice-discourse
+# url: https://github.com/choiceaustralia/discourse-payments
 
 gem 'stripe', '1.58.0'
 
-load File.expand_path('../lib/choice-discourse/engine.rb', __FILE__)
+load File.expand_path('../lib/discourse-payments/engine.rb', __FILE__)
 
 Discourse::Application.routes.prepend do
   mount ::Choice::Engine, at: '/'
