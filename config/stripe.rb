d88b473af740d1674cca78e9ccd315f6e6c8f1c9
Rimian Perkins <hello@rimian.com.au>
2017-02-16 18:13:57 +1100

    check file exists

--- config/stripe.rb
+++ config/stripe.rb
@@ -1,13 +1,13 @@
 
-if ENV['STRIPE_SECRET_KEY'].present?
-  Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key      => ENV['STRIPE_SECRET_KEY']
-  }
-else
+if File.exist?('config/stripe.yml')
   Rails.configuration.stripe = {
     :publishable_key => Rails.application.config_for(:stripe)['publishable_key'],
     :secret_key      => Rails.application.config_for(:stripe)['secret_key']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
   }
 end
 
