0e0b4526269f985eca05d890adb766d699b0ecb3
Rimian Perkins <hello@rimian.com.au>
2017-02-16 16:43:21 +1100

    check env set

--- config/stripe.rb
+++ config/stripe.rb
@@ -1,6 +1,14 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||  Rails.application.config_for(:stripe)['publishable_key'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY'] ||  Rails.application.config_for(:stripe)['secret_key']
-}
+
+if ENV['STRIPE_SECRET_KEY'].present?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => Rails.application.config_for(:stripe)['publishable_key'],
+    :secret_key      => Rails.application.config_for(:stripe)['secret_key']
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
