5dc2cccc38add7e7a9070c83cdb5fb4929a5e895
Rimian Perkins <hello@rimian.com.au>
2017-02-20 11:43:12 +1100

    bump with stripe form

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-payments
 # about: Integrating Discourse with Stripe
-# version: 1.1.0
+# version: 1.2.0
 # url: https://github.com/choiceaustralia/discourse-payments
 
 gem 'stripe', '1.58.0'
