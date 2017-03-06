011ae1b2bed995fe0f58d43aaec3a6830d968681
Rimian Perkins <hello@rimian.com.au>
2017-03-06 14:44:11 +1100

    add an extra nav item

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-donations
 # about: Integrating Discourse with Stripe for donations
-# version: 1.7.3
+# version: 1.8.0
 # url: https://github.com/choiceaustralia/discourse-donations
 # authors: Rimian Perkins
 
