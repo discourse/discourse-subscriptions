567754de922133636039e7dbb77049297e02bf5f
Rimian Perkins <rimian@users.noreply.github.com>
2017-03-16 05:09:08 +1100

    typo and translations

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-donations
 # about: Integrating Discourse with Stripe for donations
-# version: 1.9.3
+# version: 1.9.4
 # url: https://github.com/choiceaustralia/discourse-donations
 # authors: Rimian Perkins
 
