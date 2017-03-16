9ddeba4b2b269804e1186d8d71600ad6f1b1d2bf
Rimian Perkins <hello@rimian.com.au>
2017-03-16 11:56:59 +1100

    bump with old code removed

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-donations
 # about: Integrating Discourse with Stripe for donations
-# version: 1.9.4.1
+# version: 1.9.5
 # url: https://github.com/choiceaustralia/discourse-donations
 # authors: Rimian Perkins
 
