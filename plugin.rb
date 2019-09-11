b97e9b91c258e0131f3ae756ed2ac9b7412562d7
Rimian Perkins <hello@rimian.com.au>
2019-09-11 19:18:30 +1000

    add stripe gem

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,20 @@
 # name: Discourse Patrons
-# about: Integrates Stripe into Discourse to allow visitors to make donations
+# about: Integrates Stripe into Discourse to allow visitors to make payments
 # version: 1.0.0
-# url: https://github.com/rimian/discourse-patron
+# url: https://github.com/rimian/discourse-patrons
 # authors: Rimian Perkins
 
 enabled_site_setting :discourse_patrons_enabled
+
+gem 'stripe', '5.1.0'
+
+register_html_builder('server:before-head-close') do
+  "<script src='https://js.stripe.com/v3/'></script>"
+end
+
+extend_content_security_policy(
+  script_src: ['https://js.stripe.com/v3/']
+)
 
 after_initialize do
   load File.expand_path('../lib/discourse_patrons/engine.rb', __FILE__)
