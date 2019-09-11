02c9ea873aa0eb5962f45686458b7a69442d8d72
Rimian Perkins <hello@rimian.com.au>
2019-09-11 21:14:48 +1000

    add stylesheets

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,9 @@ enabled_site_setting :discourse_patrons_enabled
 
 gem 'stripe', '5.1.1'
 
+register_asset "stylesheets/common/discourse-patrons.scss"
+register_asset "stylesheets/mobile/discourse-patrons.scss"
+
 register_html_builder('server:before-head-close') do
   "<script src='https://js.stripe.com/v3/'></script>"
 end
@@ -19,6 +22,9 @@ extend_content_security_policy(
 )
 
 after_initialize do
+  ::Stripe.api_version = "2019-08-14"
+  ::Stripe.set_app_info('Discourse Patrons', version: '1.0.0', url: 'https://github.com/rimian/discourse-patrons')
+
   load File.expand_path('../lib/discourse_patrons/engine.rb', __FILE__)
   load File.expand_path('../config/routes.rb', __FILE__)
   load File.expand_path('../app/controllers/patrons_controller.rb', __FILE__)
