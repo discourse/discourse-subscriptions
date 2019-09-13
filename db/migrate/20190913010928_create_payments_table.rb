d09f654b3ec7b75df70ae31211fe88f45fb613a9
Rimian Perkins <hello@rimian.com.au>
2019-09-13 14:35:13 +1000

    add migration

--- db/migrate/20190913010928_create_payments_table.rb
+++ db/migrate/20190913010928_create_payments_table.rb
@@ -2,16 +2,10 @@
 
 class CreatePaymentsTable < ActiveRecord::Migration[5.2]
   def change
-    def change
-      create_table :payments do |t|
-        t.integer :payment_intent_id, null: false
-        t.string :receipt_email, null: false
-        t.string :url, null: false
-        t.integer :amount, null: false
-        t.timestamps
-      end
-
-      add_index :payments, [:payment_intent_id], unique: true
+    create_table :payments do |t|
+      t.timestamps
     end
+
+    add_index :payments, [:payment_intent_id], unique: true
   end
 end
