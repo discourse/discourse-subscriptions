8f4cb95cb53268c282624424f302904a561097e5
Rimian Perkins <hello@rimian.com.au>
2019-09-13 22:20:29 +1000

    string for payment intent id

--- db/migrate/20190913010928_create_payments_table.rb
+++ db/migrate/20190913010928_create_payments_table.rb
@@ -3,7 +3,7 @@
 class CreatePaymentsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :payments do |t|
-      t.integer :payment_intent_id, null: false
+      t.string :payment_intent_id, null: false
       t.string :receipt_email, null: false
       t.string :url, null: false
       t.integer :amount, null: false
