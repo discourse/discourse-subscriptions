cf46065635563aa5e8acd958f344825c482af950
Rimian Perkins <hello@rimian.com.au>
2019-09-14 16:34:51 +1000

    show username

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class PaymentSerializer < ApplicationSerializer
-  attributes :payment_intent_id, :receipt_email, :url, :created_at_age, :amount, :amount_currency
+  attributes  :payment_intent_id,
+              :receipt_email,
+              :url,
+              :created_at_age,
+              :amount,
+              :amount_currency,
+              :username
 
   def created_at_age
     Time.now - object.created_at
@@ -9,13 +15,25 @@ class PaymentSerializer < ApplicationSerializer
 
   def amount_currency
     ActiveSupport::NumberHelper.number_to_currency(
-      object.amount/100,
+      object.amount / 100,
       precision: 2,
       unit: currency_unit
     )
   end
 
+  def username
+    user&.username
+  end
+
   private
+
+  def user
+    begin
+      User.find(object.user_id)
+    rescue
+      nil
+    end
+  end
 
   def currency_unit
     case object.currency
