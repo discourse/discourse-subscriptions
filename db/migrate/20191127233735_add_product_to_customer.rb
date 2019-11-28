1429bf116f5926b2578b79f836d6fb37ce1da36e
Rimian Perkins <hello@rimian.com.au>
2019-11-28 19:51:03 +1100

    rubocop

--- db/migrate/20191127233735_add_product_to_customer.rb
+++ db/migrate/20191127233735_add_product_to_customer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddProductToCustomer < ActiveRecord::Migration[6.0]
   def change
     add_column :discourse_patrons_customers, :product_id, :string
--- db/migrate/20191128044206_remove_customer_index.rb
+++ db/migrate/20191128044206_remove_customer_index.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveCustomerIndex < ActiveRecord::Migration[6.0]
   def change
     remove_index :discourse_patrons_customers, :customer_id
