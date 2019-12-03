949e32e4fb2350c7075cc50764fdf04237b52459
Rimian Perkins <hello@rimian.com.au>
2019-12-03 11:01:44 +1100

    fix path to fabricators

--- spec/plugin_helper.rb
+++ spec/plugin_helper.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join("plugins/discourse-patrons/spec/fabricators/*.rb")].each { |f| require f }
+Dir[Rails.root.join("plugins/discourse-subscriptions/spec/fabricators/*.rb")].each { |f| require f }
