e3d2ac0101165c25691032ef1bca958cc769af85
Rimian Perkins <hello@rimian.com.au>
2019-12-05 17:11:05 +1100

    rm old migrations

--- db/migrate/20191127233735_add_product_to_customer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddProductToCustomer < ActiveRecord::Migration[6.0]
-  def change
-    add_column :discourse_patrons_customers, :product_id, :string
-  end
-end
--- db/migrate/20191128044206_remove_customer_index.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveCustomerIndex < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :discourse_patrons_customers, :customer_id
-  end
-end
