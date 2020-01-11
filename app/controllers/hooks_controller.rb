0856381fbee77e1cc8d292a53d2c9678ee0c41e5
Rimian Perkins <hello@rimian.com.au>
2020-01-12 09:33:26 +1100

    skip authenticity check on hooks controller

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,6 +2,8 @@
 
 module DiscourseSubscriptions
   class HooksController < ::ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+
     def create
       head 200
     end
