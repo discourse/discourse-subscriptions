0543b3a6a39029b6dd6adece0224c7abe1ea8e05
Rimian Perkins <hello@rimian.com.au>
2020-01-13 11:10:06 +1100

    basic hook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,6 +3,25 @@
 module DiscourseSubscriptions
   class HooksController < ::ApplicationController
     def create
+      begin
+
+        # payload, sig_header, endpoint_secret
+        event = ::Stripe::Webhook.construct_event(
+          {},
+          'stripe-webhook-signature',
+          'endpoint_secret'
+        )
+
+      rescue JSON::ParserError => e
+        # Invalid payload
+        status 400
+        return
+      rescue Stripe::SignatureVerificationError => e
+        # Invalid signature
+        status 400
+        return
+      end
+
       head 200
     end
   end
--- spec/requests/hooks_controller_spec.rb
+++ spec/requests/hooks_controller_spec.rb
@@ -4,8 +4,18 @@ require 'rails_helper'
 
 module DiscourseSubscriptions
   RSpec.describe HooksController do
-    it "responds ok" do
+    it "contructs a webhook event" do
+      ::Stripe::Webhook
+        .expects(:construct_event)
+        .with({}, 'stripe-webhook-signature', 'endpoint_secret')
+        .returns(true)
+
+      headers = {
+        'HTTP_STRIPE_SIGNATURE' => 'stripe-webhook-signature'
+      }
+
       post "/s/hooks.json"
+
       expect(response.status).to eq 200
     end
   end
