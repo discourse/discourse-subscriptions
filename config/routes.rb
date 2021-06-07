1121e6f7ce43ef7b2ddd047287a809c1ff75424f
Justin DiRose <justin@justindirose.com>
2021-06-07 10:31:34 -0500

    FIX: Restrict mods from seeing Subscriptions admin features

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ DiscourseSubscriptions::Engine.routes.draw do
   end
 
   namespace :admin do
-    resources :plans
-    resources :subscriptions, only: [:index, :destroy]
-    resources :products
-    resources :coupons, only: [:index, :create]
-    resource :coupons, only: [:destroy, :update]
+    resources :plans, constraints: AdminConstraint.new
+    resources :subscriptions, only: [:index, :destroy], constraints: AdminConstraint.new
+    resources :products, constraints: AdminConstraint.new
+    resources :coupons, only: [:index, :create], constraints: AdminConstraint.new
+    resource :coupons, only: [:destroy, :update], constraints: AdminConstraint.new
   end
 
   namespace :user do
