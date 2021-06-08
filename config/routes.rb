39f3c52d8fa69e7528059ce0fef221a515a25c08
Justin DiRose <justin@justindirose.com>
2021-06-08 13:27:27 -0500

    DRY up route constraint

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ DiscourseSubscriptions::Engine.routes.draw do
     post '/create-campaign' => 'admin#create_campaign'
   end
 
-  namespace :admin do
-    resources :plans, constraints: AdminConstraint.new
-    resources :subscriptions, only: [:index, :destroy], constraints: AdminConstraint.new
-    resources :products, constraints: AdminConstraint.new
-    resources :coupons, only: [:index, :create], constraints: AdminConstraint.new
-    resource :coupons, only: [:destroy, :update], constraints: AdminConstraint.new
+  namespace :admin, constraints: AdminConstraint.new do
+    resources :plans
+    resources :subscriptions, only: [:index, :destroy]
+    resources :products
+    resources :coupons, only: [:index, :create]
+    resource :coupons, only: [:destroy, :update]
   end
 
   namespace :user do
