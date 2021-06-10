d62a6b3d290aac232e2af3f90b96aeec395aefc1
Jordan Vidrine <jordan@jordanvidrine.com>
2021-06-10 14:30:30 -0500

    UX: Adjust mobile styling

--- assets/javascripts/discourse/connectors/after-topic-footer-buttons/subscriptions-campaign-topic-footer.js
+++ assets/javascripts/discourse/connectors/after-topic-footer-buttons/subscriptions-campaign-topic-footer.js
@@ -1,7 +1,6 @@
 export default {
   shouldRender(args, component) {
     const { siteSettings } = component;
-    const mobileView = component.site.mobileView;
     const bannerLocation =
       siteSettings.discourse_subscriptions_campaign_banner_location;
 
