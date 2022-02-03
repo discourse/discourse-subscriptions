eee6932e2a57d11058f5b2ceac2f3a0e4bb76a00
Vinoth Kannan <svkn.87@gmail.com>
2022-02-03 23:16:43 +0530

    DEV: execute campaign refresh job only if stripe is configured.

--- app/jobs/scheduled/refresh_subscriptions_campaign_data.rb
+++ app/jobs/scheduled/refresh_subscriptions_campaign_data.rb
@@ -2,10 +2,11 @@
 
 module ::Jobs
   class RefreshSubscriptionsCampaignData < ::Jobs::Scheduled
+    include ::DiscourseSubscriptions::Stripe
     every 30.minutes
 
     def execute(args)
-      return unless SiteSetting.discourse_subscriptions_campaign_enabled
+      return unless SiteSetting.discourse_subscriptions_campaign_enabled && is_stripe_configured?
       DiscourseSubscriptions::Campaign.new.refresh_data
     end
   end
