6f70af5e4210e8ea9a75b389717d07094520b549
Vinoth Kannan <svkn.87@gmail.com>
2022-02-04 11:06:40 +0530

    DEV: execute campaign refresh job only if stripe is configured. (#108)

---
+++ spec/jobs/refresh_subscriptions_campaign_data_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Jobs::RefreshSubscriptionsCampaignData do
+
+  before do
+    SiteSetting.discourse_subscriptions_campaign_enabled = true
+  end
+
+  it 'should execute the job only if stripe is configured' do
+    DiscourseSubscriptions::Campaign.any_instance.expects(:refresh_data).once
+    described_class.new.execute({})
+
+    SiteSetting.discourse_subscriptions_public_key = "PUBLIC_KEY"
+    SiteSetting.discourse_subscriptions_secret_key = "SECRET_KEY"
+    described_class.new.execute({})
+  end
+
+end
