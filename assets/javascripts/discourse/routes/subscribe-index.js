2babb43ffbbf3416514d56a125d222a0f4f99b6e
Selase Krakani <849886+s3lase@users.noreply.github.com>
2023-05-04 13:52:04 +0000

    FEATURE: Skip product listing if only one product is present (#160)
    At the moment, paying for a product typically involves first clicking
    the subscribe link added to the navigation bar, selecting a product and
    then adding card details to to make the purchase.
    
    This change skips the product selection step if the site has only one
    product.

--- assets/javascripts/discourse/routes/subscribe-index.js
+++ assets/javascripts/discourse/routes/subscribe-index.js
@@ -5,4 +5,19 @@ export default Route.extend({
   model() {
     return Product.findAll();
   },
+
+  afterModel(products) {
+    if (products.length === 1) {
+      const product = products[0];
+
+      if (this.currentUser && product.subscribed && !product.repurchaseable) {
+        this.transitionTo(
+          "user.billing.subscriptions",
+          this.currentUser.username
+        );
+      } else {
+        this.transitionTo("subscribe.show", product.id);
+      }
+    }
+  },
 });
