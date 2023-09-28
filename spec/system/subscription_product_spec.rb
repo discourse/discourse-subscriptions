c30fea21efdd9bd7dccf5849f856daab716e5f35
Natalie Tay <natalie.tay@gmail.com>
2023-09-28 14:21:16 +0800

    FIX: Clicking on login should show login modal (#177)
    * FIX: Clicking on login should show login modal
    
    * Add system test that shows the login modal appears

---
+++ spec/system/subscription_product_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe "Subscription products", type: :system do
+  fab!(:admin) { Fabricate(:admin) }
+  fab!(:product) { Fabricate(:product, external_id: "prod_OiKyO6ZMFCIhQa") }
+
+  before { SiteSetting.discourse_subscriptions_enabled = true }
+
+  it "shows the login modal" do
+    visit("/s")
+
+    find("button.login-required.subscriptions").click
+
+    expect(page).to have_css(".modal-container .login-modal")
+  end
+end
