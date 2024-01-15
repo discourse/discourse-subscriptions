76da899d51bc24f978f96dc250ba4e71e3fd01a1
David Taylor <david@taylorhq.com>
2024-01-15 11:08:16 +0000

    DEV: Resolve registerUnbound deprecation

--- assets/javascripts/discourse/helpers/format-unix-date.js
+++ assets/javascripts/discourse/helpers/format-unix-date.js
@@ -1,8 +1,7 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
 import { autoUpdatingRelativeAge } from "discourse/lib/formatter";
 import { htmlSafe } from "@ember/template";
 
-registerUnbound("format-unix-date", function (timestamp) {
+export default function formatUnixDate(timestamp) {
   if (timestamp) {
     const date = new Date(moment.unix(timestamp).format());
 
@@ -14,4 +13,4 @@ registerUnbound("format-unix-date", function (timestamp) {
       })
     );
   }
-});
+}
