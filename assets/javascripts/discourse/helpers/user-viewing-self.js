a3883d2a9a697619e6e0db9b2538e3f6a252da98
David Taylor <david@taylorhq.com>
2024-01-15 11:52:19 +0000

    DEV: Resolve registerUnbound deprecation (#188)

--- assets/javascripts/discourse/helpers/user-viewing-self.js
+++ assets/javascripts/discourse/helpers/user-viewing-self.js
@@ -1,7 +1,6 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
 import User from "discourse/models/user";
 
-export default registerUnbound("user-viewing-self", function (model) {
+export default function userViewingSelf(model) {
   if (User.current()) {
     return (
       User.current().username.toLowerCase() === model.username.toLowerCase()
@@ -9,4 +8,4 @@ export default registerUnbound("user-viewing-self", function (model) {
   }
 
   return false;
-});
+}
