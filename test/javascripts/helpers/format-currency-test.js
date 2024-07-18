49d4142731913be1cb59a9de190878d2aecb53ab
Blake Erickson <o.blakeerickson@gmail.com>
2024-07-18 15:55:07 -0600

    lint test

--- test/javascripts/helpers/format-currency-test.js
+++ test/javascripts/helpers/format-currency-test.js
@@ -1,12 +1,12 @@
-import { setupTest } from 'ember-qunit';
-import { module, test } from 'qunit';
-import { formatCurrency } from 'discourse/plugins/discourse-subscriptions/discourse/helpers/format-currency';
+import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
+import { formatCurrency } from "discourse/plugins/discourse-subscriptions/discourse/helpers/format-currency";
 
-module('Unit | Helper | format-currency', function(hooks) {
+module("Unit | Helper | format-currency", function (hooks) {
   setupTest(hooks);
 
-  test('it formats USD correctly', function(assert) {
+  test("it formats USD correctly", function (assert) {
     let result = formatCurrency(["USD", 338.2]);
-    assert.equal(result, '$338.20', 'Formats USD correctly');
+    assert.equal(result, "$338.20", "Formats USD correctly");
   });
 });
