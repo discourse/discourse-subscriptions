7b809d68d0dfd01fa8a7eb1d6bd350a913823b8f
Blake Erickson <o.blakeerickson@gmail.com>
2024-10-02 17:15:36 -0600

    prettier

--- assets/javascripts/discourse/routes/subscribe.js
+++ assets/javascripts/discourse/routes/subscribe.js
@@ -1,14 +1,15 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default class SubscribeRoute extends Route {
   @service router;
 
   beforeModel() {
-    const pricingTableEnabled = this.siteSettings.discourse_subscriptions_pricing_table_enabled;
+    const pricingTableEnabled =
+      this.siteSettings.discourse_subscriptions_pricing_table_enabled;
 
     if (pricingTableEnabled) {
-      this.router.transitionTo('subscriptions');
+      this.router.transitionTo("subscriptions");
     }
   }
 }
