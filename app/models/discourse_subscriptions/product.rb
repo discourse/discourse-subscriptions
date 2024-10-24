8f8baf05fb3b430792291372f7b81514f34099db
Gabriel Grubba <gabriel@discourse.org>
2024-10-24 17:10:49 -0300

    DEV: Update plugin annotations

--- app/models/discourse_subscriptions/product.rb
+++ app/models/discourse_subscriptions/product.rb
@@ -4,3 +4,17 @@ module DiscourseSubscriptions
   class Product < ActiveRecord::Base
   end
 end
+
+# == Schema Information
+#
+# Table name: discourse_subscriptions_products
+#
+#  id          :bigint           not null, primary key
+#  external_id :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_discourse_subscriptions_products_on_external_id  (external_id)
+#
