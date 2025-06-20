fe53f2cbc0e9d4032860f03b3f4ab878e6869613
Jarek Radosz <jradosz@gmail.com>
2025-06-20 21:36:54 +0200

    DEV: Clean up helpers (#279)
    * remove an unused helper
    * move a single-use helper to its call-site as a getter
    * convert `format-currency` to a pure function helper

--- test/javascripts/unit/helpers/format-currency-test.js
+++ test/javascripts/unit/helpers/format-currency-test.js
@@ -1,17 +1,17 @@
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
-import { formatCurrency } from "discourse/plugins/discourse-subscriptions/discourse/helpers/format-currency";
+import formatCurrency from "discourse/plugins/discourse-subscriptions/discourse/helpers/format-currency";
 
 module("Subscriptions | Unit | Helper | format-currency", function (hooks) {
   setupTest(hooks);
 
-  test("it formats USD correctly", function (assert) {
-    let result = formatCurrency(["USD", 338.2]);
+  test("formats USD correctly", function (assert) {
+    const result = formatCurrency("USD", 338.2);
     assert.equal(result, "$338.20", "Formats USD correctly");
   });
 
-  test("it rounds correctly", function (assert) {
-    let result = formatCurrency(["USD", 338.289]);
+  test("rounds correctly", function (assert) {
+    const result = formatCurrency("USD", 338.289);
     assert.equal(result, "$338.29", "Rounds correctly");
   });
 });
